4300a5edc4ea19ab34c32eeaf6f31fac46aafd49
JustMargata <66246000+JustMargata@users.noreply.github.com>
2024-11-05 12:23:07 +0200

    Added files to repository

---
+++ InformacionnaSlujba/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InformacionnaSlujba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Obekt s ime i godini");
+                Slujba slujba = new Slujba("Gosho", 23);
+                slujba.PrintNameAndAge();
+                Console.WriteLine(new string('-', 20));
+                Console.Write("Kolko choveka shte vuvejdash: ");
+                int count = int.Parse(Console.ReadLine());
+                string[] data;
+                Console.WriteLine("Vuvedi grad, ime, godini, pol, adres na 1 red");
+                List<Slujba> list = new List<Slujba>();
+                for (int i = 0; i < count; i++)
+                {
+                    data = Console.ReadLine().Split();
+                    Slujba slujba1 = new Slujba(data[0], data[1], int.Parse(data[2]), data[3], data[4]);
+                    list.Add(slujba1);
+                }
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Vsichki vuvedeni hora");
+                list.ForEach(x => x.Print());
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Vuvedi ime na koeto iskash da vidish dannite");
+                string name = Console.ReadLine();
+                list.Where(x => x.Name == name).ToList().ForEach(x => x.Print());
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Nai-Mladiqt");
+                list.OrderBy(x => x.Age).Take(1).ToList().ForEach(x => x.Print());
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Vsichki hora podredeni po ime");
+                list.OrderBy(x => x.Name).ToList().ForEach(x => x.Print());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ InformacionnaSlujba/Slujba.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformacionnaSlujba
+{
+    internal class Slujba
+    {
+        private string city;
+        private string name;
+        private int age;
+        private string gender;
+        private string adress;
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 18 || value > 60)
+                {
+                    throw new ArgumentOutOfRangeException("Godinite trqbva da sa mejdu 18 i 60");
+                }
+                age = value;
+            }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+
+        public Slujba(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public Slujba(string city, string name, int age, string gender, string adress)
+        {
+            this.City = city;
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+            this.Adress = adress;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Grad: {this.City} Ime: {this.Name} Godini: {this.Age} Pol: {this.Gender} Adres: {this.Adress}");
+        }
+
+        internal void PrintNameAndAge()
+        {
+            Console.WriteLine($"Ime: {this.Name} Godini: {this.Age}");
+        }
+    }
+}
